07c0800260ffdf41224cbdbee6c37b8bfd85e913
Rajendra Thorat <raju.thorat@gmail.com>
2024-11-23 17:50:18 +0530

    Initial commit

---
+++ src/test/java/com/raj/jdemo/JdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.jdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
